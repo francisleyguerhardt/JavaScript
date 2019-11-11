6c716497172788b934384397f7c70fd615a298c9
francisleyguerhardt <57117300+francisleyguerhardt@users.noreply.github.com>
2019-11-11 17:33:25 -0300

    EtanolXGasolina

---
+++ etanolXgasolina/combustivel.js
@@ -0,0 +1,12 @@
+/**
+/ Calculadora Flex
+/@author Francisley Guerhardt
+*/
+
+let etanol, gasolina 
+
+function calcular (){
+    etanol = parseFloat(frmCalc.txtEtanol.value.replace(",","."))
+    gasolina = parseFloat(frmCalc.txtGasolina.value.replace(",","."))
+    if(etanol / 0,7 = )
+}
