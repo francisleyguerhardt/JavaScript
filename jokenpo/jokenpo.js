3303c320ce1f3268a9476dc28eaedcbae96740d9
francisleyguerhardt <57117300+francisleyguerhardt@users.noreply.github.com>
2019-11-13 17:34:27 -0300

    jokenPo

--- jokenpo/jokenpo.js
+++ jokenpo/jokenpo.js
@@ -3,27 +3,28 @@
  * @author Francisley Guerhardt
  */
 
-
 function jogar() {
     let sorteio = Math.floor(Math.random() * 3)
-        switch (sorteio) {
-            case 0:
+    switch (sorteio) {
+        case 0:
             document.getElementById('pc').src = 'pcpedra.png'
             break
-            case 1:
+        case 1:
             document.getElementById('pc').src = 'pcpapel.png'
             break
-            case 2:
+        case 2:
             document.getElementById('pc').src = 'pctesoura.png'
             break
-        }
-        if ((document.getElementById('pedra').checked === true && sorteio === 0) || (document.getElementById('papel').checked === true && sorteio === 1) || (document.getElementById('tesoura').checked === true && sorteio === 2))
-        document.getElementById('resultado').innerText='EMPATE'
-        else if((document.getElementById('pedra').checked === true && sorteio === 0) || (document.getElementById('papel').checked === true && sorteio === 1) || (document.getElementById('tesoura').checked === true && sorteio === 2))
-        document.getElementById('resultado').innerText='jOGADOR GANHOU'
-        else if((document.getElementById('pedra').checked === true && sorteio === 0) || (document.getElementById('papel').checked === true && sorteio === 1) || (document.getElementById('tesoura').checked === true && sorteio === 2))
-        document.getElementById('resultado').innerText='COMPUTADOR GANHOU'
     }
+    if ((document.getElementById('pedra').checked === true && sorteio === 0) || (document.getElementById('papel').checked === true && sorteio === 1) || (document.getElementById('tesoura').checked === true && sorteio === 2))
+        document.getElementById('resultado').innerText = 'EMPATE'
+    else if ((document.getElementById('pedra').checked == true && sorteio === 2) || (document.getElementById('papel').checked === true && sorteio === 0) || (document.getElementById('tesoura').checked === true && sorteio === 1))
+        document.getElementById('resultado').innerText = 'JOGADOR GANHOU'
+    else if ((document.getElementById('tesoura').checked === true && sorteio === 0) || (document.getElementById('pedra').checked === true && sorteio === 1) || (document.getElementById('papel').checked === true && sorteio === 2))
+        document.getElementById('resultado').innerText = 'COMPUTADOR GANHOU'
+}
 
-
-
+function resetar() {
+    document.getElementById('pc').src = 'pc.png'
+    document.getElementById('resultado').innerText = ''
+}
